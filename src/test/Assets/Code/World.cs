27f188e79a32045c4fd63adc9061d1158cfb3d7f
Crackers0106 <32655060+Crackers0106@users.noreply.github.com>
2019-09-07 20:18:59 -0400

    First Commit

---
+++ src/test/Assets/Code/World.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject ChunkGO;
+    void Start() {
+
+        for (int i = 0; i < 2; i++){
+           for (int j = 0; j < 2; j++){
+                Instantiate(ChunkGO);
+            }
+        }
+    }            
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
